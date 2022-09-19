a4641703833aad5edb4cb2b40d3d43091745b54c
wyattlang <63122114+wyattlang@users.noreply.github.com>
2022-09-19 05:28:36 -0400

    Add files via upload

---
+++ mess.java
@@ -0,0 +1,32 @@
+
+
+
+/*
+  in a hurry, nottoime to cleanup after myaself, but
+  I'll do it later for usre.
+  /* right... */
+*/
+class clean_me {
+
+
+public static void Main(string[]
+commandline_arguments) {
+
+    int housenumber_butNOTincludingtheAPTnumber = 456;
+    string 1STREET  =
+      "Fell St.";
+System.Out.Println(housenumber_butNOTincludingtheAPTnumber);
+
+  system.printout = 1STREET;  }
+
+
+
+/*
+  // i'll finish this later
+*/
+void AnotherMethod()
+{
+  int goto = 1;
+  }   // end of class
+
+}
